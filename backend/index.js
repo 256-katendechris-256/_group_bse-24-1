c6e52f60320adaa6a7781e7021af8cb1d414f978
Martin <maertinmugenyi26@gmail.com>
2024-09-27 16:52:11 +0300

    modified the index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const { default: axios } = require("axios");
+import express from "express";
+import cors from "cors";
+import axios from "axios";
 
 const app = express();
 app.use(express.json());
@@ -12,17 +12,15 @@ app.post("/authenticate", async (req, res) => {
   try {
     const response = await axios.put(
       'https://api.chatengine.io/users/',
-      {username: username, secret: username, first_name: username },
+      { username: username, secret: username, first_name: username },
       { headers: { "Private-Key": "4dd14d0f-b767-4d5d-b220-f92ec584b879" } }
     );
     return res.send(response.data);
   } catch (e) {
     return res.status(404).json(e.response.data);
   }
-  
- 
 });
 
-app.listen(3000, ()=> {
-  console.log('Server is running on http://localhost:3000')
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
