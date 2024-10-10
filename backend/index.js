d1b5058444fab0a218b3ad4e675eb2ec9c6fc1c2
Katende Chris Marvin <85847700+256-katendechris-256@users.noreply.github.com>
2024-10-10 07:05:20 +0300

    Serve Vite-built frontend from backendh0h8i

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import axios from "axios";
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -8,34 +8,29 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: true }));
-
-// Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'public')));
 
-// API routes
-app.post("/authenticate", async (req, res) => {
+app.post('/authenticate', async (req, res) => {
   const { username } = req.body;
-
   try {
-    const response = await axios.put(
+    const r = await axios.put(
       'https://api.chatengine.io/users/',
       { username: username, secret: username, first_name: username },
-      { headers: { "Private-Key": process.env.CHATENGINE_PRIVATE_KEY } }
+      { headers: { "Private-Key": process.env.CHAT_ENGINE_PRIVATE_KEY } }
     );
-    return res.send(response.data);
+    return res.status(r.status).json(r.data);
   } catch (e) {
-    return res.status(404).json(e.response.data);
+    return res.status(e.response.status).json(e.response.data);
   }
 });
 
-// Anything that doesn't match the above, send back the index.html 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
